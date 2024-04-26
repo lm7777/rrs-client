534984e60312214c8dbdb2b3db5c7a717f549088
g.martisius <g.martisius@cid.com>
2024-04-26 11:45:05 +0300

    Folder structure, recipe list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DemoComponent } from './demo/demo/demo.component';
-import {RecipeDetailsComponent} from "./recipe-details/recipe-details.component";
+import { RecipeDetailsComponent } from "./recipes/recipe-details/recipe-details.component";
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 
 export const routes: Routes = [
-    { path: 'demo', component: DemoComponent },
-    { path: 'recipe/:id', component: RecipeDetailsComponent },
+    {path: '', component: RecipesListComponent},
+    {path: 'demo', component: DemoComponent},
+    {path: 'recipe/:id', component: RecipeDetailsComponent},
 ];
---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rrs-recipes-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipes-list.component.html',
+  styleUrl: './recipes-list.component.scss'
+})
+export class RecipesListComponent {
+
+}
