a7c5411ba9d447bfa07674fbe56fbe6bcbcbf9e9
Rūta Galinkevičė <r.galinkevice@cid.com>
2024-04-26 08:43:59 +0300

    Temporary recipe details page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DemoComponent } from './demo/demo/demo.component';
+import {RecipeDetailsComponent} from "./recipe-details/recipe-details.component";
 
 export const routes: Routes = [
-  { path: 'demo', component: DemoComponent }
+    { path: 'demo', component: DemoComponent },
+    { path: 'recipe/:id', component: RecipeDetailsComponent },
 ];
---
+++ src/app/recipe-details/recipe-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rrs-recipe-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-details.component.html',
+  styleUrl: './recipe-details.component.scss'
+})
+export class RecipeDetailsComponent {
+}
