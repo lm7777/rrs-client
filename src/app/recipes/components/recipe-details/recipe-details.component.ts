0ca19c457c3f3287a275c357f6168af13dbf2266
Rūta Galinkevičė <r.galinkevice@cid.com>
2024-05-08 15:38:24 +0300

    Small fixes and save checked ingredients to local storage

--- src/app/recipes/components/recipe-details/recipe-details.component.ts
+++ src/app/recipes/components/recipe-details/recipe-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from "@angular/router";
 import { Recipe } from "../../data/recipe.model";
 import { RecipeService } from "../../services/recipe.service";
@@ -19,10 +19,12 @@ import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
     templateUrl: './recipe-details.component.html',
     styleUrl: './recipe-details.component.scss'
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
     recipe: Recipe = new Recipe();
     user: User = new User();
     recipeBookmarked: boolean;
+    selectedItems: any[] = [];
+    checkedArray:any[] = [];
 
     constructor(private route: ActivatedRoute,
                 private userService: UserService,
@@ -39,6 +41,18 @@ export class RecipeDetailsComponent {
                 this.recipe = result;
                 this.recipeBookmarked = this.isRecipeBookmarked();
             });
+
+        this.selectedItems = JSON.parse(localStorage.getItem('checkedIngredients' + this.recipe.id))
+    }
+
+    ngOnInit() {
+        this.checkedArray = this.recipe.ingredients.map(ingredient => ({
+            category: ingredient.category,
+            items: ingredient.items.map(item => ({
+                name: item.name,
+                checked: this.checkIfIngredientChecked(ingredient.category, item.name)
+            }))
+        }));
     }
 
     toggleBookmarkRecipe() {
@@ -58,5 +72,38 @@ export class RecipeDetailsComponent {
 
     scroll(el: HTMLElement) {
         el.scrollIntoView({behavior: 'smooth'});
+    }
+
+    onCheckIngredient(category: string, ingredient: string) {
+        let ingredients: any[] = JSON.parse(localStorage.getItem('checkedIngredients' + this.recipe.id) || '[]');
+        const categoryIndex: number = ingredients.findIndex((item) => item.category === category);
+
+        if (categoryIndex === -1) {
+            ingredients.push({
+                recipeId: this.recipe.id,
+                category: category,
+                items: [{ name: ingredient }]
+            });
+        } else {
+            const itemIndex: number = ingredients[categoryIndex].items.findIndex((item: { name: string }) => item.name === ingredient);
+
+            if (itemIndex === -1) {
+                ingredients[categoryIndex].items.push({ name: ingredient });
+            } else {
+                ingredients[categoryIndex].items.splice(itemIndex, 1);
+            }
+
+            if (ingredients[categoryIndex].items.length === 0) {
+                ingredients.splice(categoryIndex, 1);
+            }
+        }
+        localStorage.setItem('checkedIngredients' + this.recipe.id, JSON.stringify(ingredients));
+    }
+
+    checkIfIngredientChecked(category: string, ingredient: string) {
+        if (!this.selectedItems) return false;
+        const categoryItems = this.selectedItems.find(item => item.category === category)?.items;
+        if (!categoryItems) return false;
+        return categoryItems.some((item: { name: string }) => item.name === ingredient);
     }
 }
